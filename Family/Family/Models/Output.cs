e8a92ed46fbb7324237cc9510d66450b0cdf9d36
coolMukeshbabu <82643895+coolMukeshbabu@users.noreply.github.com>
2021-04-19 18:37:01 +0530

    Added output class

---
+++ Family/Family/Models/Output.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Family.Models
+{
+    class Output
+    {
+        public string status { get; set; }
+        public string message { get; set; }
+        public List<Member> data { get; set; }
+    }
+}
